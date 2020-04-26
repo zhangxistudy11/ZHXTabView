706e236bbe1d7f0585c7ebd681fd2a2cd693f315
ZhangXi <xiza@ctrip.com>
2020-04-26 11:25:35 +0800

    chore:remove the storyboadr and use pure code

---
+++ ZHXTabView/ZHXTabView/Demo/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  ZHXTabView
+//
+//  Created by 张玺 on 2020/4/26.
+//  Copyright © 2020 张玺. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"ZHXTabView";
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
